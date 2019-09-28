15ecc03a87477a716c91ba8820c79782067ebb61
Kouji Matsui <k@kekyo.net>
2019-09-28 18:52:50 +0900

    Added bad practice sample.

---
+++ CompletedAsync/IndependentLibrary/Downloader.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentLibrary
+{
+    public interface IDownloader
+    {
+        Task<JToken> DownloadJsonAsync(string url);
+    }
+
+    public sealed class Downloader : IDownloader
+    {
+        public async Task<JToken> DownloadJsonAsync(string url)
+        {
+            var httpClient = new HttpClient();
+            using (var hs = await httpClient.GetStreamAsync(url))
+            {
+                var jr = new JsonTextReader(new StreamReader(hs, Encoding.UTF8));
+                var serializer = new JsonSerializer();
+                return serializer.Deserialize<JToken>(jr);
+            }
+        }
+    }
+}
---
+++ CompletedAsync/IndependentLibrary/InterestingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentLibrary
+{
+    public interface IService
+    {
+        void Execute(int parameter);
+    }
+
+    public sealed class LoggingService : IService
+    {
+        public void Execute(int parameter) =>
+            Console.WriteLine($"Parameter={parameter}");
+    }
+
+    public sealed class BackgroundLoggingService : IService
+    {
+        public void Execute(int parameter) =>
+            Task.Run(() => Console.WriteLine($"Parameter={parameter}"));
+    }
+
+    public sealed class MakeMoneyService : IService
+    {
+        public async void Execute(int parameter)
+        {
+            var httpClient = new HttpClient();
+            using (var response = await httpClient.PostAsync(
+                walletServiceUrl, new StringContent($"money:{parameter}")))
+            {
+                Trace.WriteLine($"Result={response.StatusCode}");
+            }
+        }
+
+        private static readonly string walletServiceUrl = "https://example.com/api/make";
+    }
+}
