2829aea7cd5c64cacd6f7e5db71481f0008d1c00
Kouji Matsui <k@kekyo.net>
2019-09-29 18:06:44 +0900

    Added WpfAppRaceCondition sample.

---
+++ CompletedAsync/RaceConditionWithAsync/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace RaceConditionWithAsync
+{
+    class Program
+    {
+        private static async Task ReadIncrementAndWriteAsync()
+        {
+            using (var fs = new FileStream("data.txt",
+                FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 65536, true))
+            {
+                var tr = new StreamReader(fs, Encoding.UTF8);
+                var value = int.TryParse(await tr.ReadToEndAsync(), out var v) ? (v + 1) : 0;
+                fs.Position = 0;
+                var tw = new StreamWriter(fs, Encoding.UTF8);
+                await tw.WriteLineAsync(value.ToString());
+                await tw.FlushAsync();
+            }
+        }
+
+        //static Task Main(string[] args) =>
+        //    Task.WhenAll(Enumerable.Range(0, 100).Select(_ => ReadIncrementAndWriteAsync()));
+
+        private static readonly object monitorLocker = new object();
+
+        private static async Task ReadIncrementAndWriteAsyncLock()
+        {
+            // lock (monitorLocker)
+            Monitor.Enter(monitorLocker);
+            try
+            {
+                using (var fs = new FileStream("data.txt",
+                    FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 65536, true))
+                {
+                    var tr = new StreamReader(fs, Encoding.UTF8);
+                    var value = int.TryParse(await tr.ReadToEndAsync(), out var v) ? (v + 1) : 0;
+                    fs.Position = 0;
+                    var tw = new StreamWriter(fs, Encoding.UTF8);
+                    await tw.WriteLineAsync(value.ToString());
+                    await tw.FlushAsync();
+                }
+            }
+            finally
+            {
+                Monitor.Exit(monitorLocker);
+            }
+        }
+
+        //static Task Main(string[] args) =>
+        //    Task.WhenAll(Enumerable.Range(0, 100).Select(_ => ReadIncrementAndWriteAsyncLock()));
+
+        private static readonly AsyncLock asyncLocker = new AsyncLock();
+
+        private static async Task ReadIncrementAndWriteAsyncLockAsync()
+        {
+            using (await asyncLocker.LockAsync())
+            {
+                using (var fs = new FileStream("data.txt",
+                    FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 65536, true))
+                {
+                    var tr = new StreamReader(fs, Encoding.UTF8);
+                    var value = int.TryParse(await tr.ReadToEndAsync(), out var v) ? (v + 1) : 0;
+                    fs.Position = 0;
+                    var tw = new StreamWriter(fs, Encoding.UTF8);
+                    await tw.WriteLineAsync(value.ToString());
+                    await tw.FlushAsync();
+                }
+            }
+        }
+
+        static Task Main(string[] args) =>
+            Task.WhenAll(Enumerable.Range(0, 100).Select(_ => ReadIncrementAndWriteAsyncLockAsync()));
+    }
+}
---
+++ CompletedAsync/WpfAppRaceConditionWithAsync/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppRaceConditionWithAsync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button.Click += this.Button_Click_NonMarshaling;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fs = new FileStream("data.txt",
+                FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 65536, true))
+            {
+                var tr = new StreamReader(fs, Encoding.UTF8);
+                var value = int.TryParse(await tr.ReadToEndAsync(), out var v) ? (v + 1) : 0;
+                fs.Position = 0;
+                var tw = new StreamWriter(fs, Encoding.UTF8);
+                await tw.WriteLineAsync(value.ToString());
+                await tw.FlushAsync();
+
+                this.textBox.AppendText($"Value={value}\r\n");
+            }
+        }
+
+        private async void Button_Click_EachTransfer(object sender, RoutedEventArgs e)
+        {
+            using (var fs = new FileStream("data.txt",
+                FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 65536, true))
+            {
+                var tr = new StreamReader(fs, Encoding.UTF8);
+                var value = int.TryParse(
+                    await Task.Run(() => tr.ReadToEnd()), out var v) ? (v + 1) : 0;
+                fs.Position = 0;
+                var tw = new StreamWriter(fs, Encoding.UTF8);
+                await Task.Run(() => tw.WriteLine(value.ToString()));
+                await Task.Run(() => tw.Flush());
+
+                this.textBox.AppendText($"Value={value}\r\n");
+            }
+        }
+
+        private async void Button_Click_NonMarshaling(object sender, RoutedEventArgs e)
+        {
+            using (var fs = new FileStream("data.txt",
+                FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, 65536, true))
+            {
+                var tr = new StreamReader(fs, Encoding.UTF8);
+                var value = int.TryParse(
+                    await tr.ReadToEndAsync().ConfigureAwait(false),
+                    out var v) ? (v + 1) : 0;
+                fs.Position = 0;
+                var tw = new StreamWriter(fs, Encoding.UTF8);
+                await tw.WriteLineAsync(value.ToString());
+                await tw.FlushAsync();
+
+                Dispatcher.Invoke(() => this.textBox.AppendText($"Value={value}\r\n"));
+            }
+        }
+    }
+}
